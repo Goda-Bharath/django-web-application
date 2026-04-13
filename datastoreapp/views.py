646d69f9afd25db3e19cd9aecf8fdbcc240126b1
godabharath2004@gmail.com <godabharath2004@gmail.com>
2026-04-13 21:08:08 +0530

    Optimized CRUD views for product management module

--- datastoreapp/views.py
+++ datastoreapp/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DeleteView, DetailView, UpdateView
-
 from .forms import productdata
 from .models import productItems
 
@@ -12,18 +10,15 @@ class ProductCreateView(CreateView):
     template_name = 'products/add_product.html'
     success_url = reverse_lazy('product_list')
 
-    def form_valid(self, form):
-        return super().form_valid(form)
-
 
 class ProductListView(ListView):
     model = productItems
     template_name = 'products/product_list.html'
     context_object_name = 'products'
-    paginate_by = 10   
+    paginate_by = 10
 
     def get_queryset(self):
-        return productItems.objects.all().order_by('-id')  
+        return productItems.objects.all().order_by('-id')
 
 
 class ProductDetailView(DetailView):
@@ -38,12 +33,8 @@ class ProductUpdateView(UpdateView):
     template_name = 'products/product_update.html'
     success_url = reverse_lazy('product_list')
 
-    def form_valid(self, form):
-        return super().form_valid(form)
-
 
 class ProductDeleteView(DeleteView):
     model = productItems
     template_name = 'products/product_delete.html'
     success_url = reverse_lazy('product_list')
-    
